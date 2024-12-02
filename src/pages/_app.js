f9b262baeb8953af8319166a71747521a7d3b19e
tsaruggan <tsaruggan@gmail.com>
2024-12-02 14:13:00 -0500

    next-pwa

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,17 @@
 import "@/styles/globals.css";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>Lift Monkey</title>
+        <meta name="description" content="weightlifting tracker" />
+        <meta name="theme-color" content="#ffffff" />
+        <link rel="manifest" href="/manifest.json" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
