1f18fa64a16a436560c8069bb5525166a01f2c81
tsaruggan <tsaruggan@gmail.com>
2025-01-09 16:14:37 -0500

    header tweaks

--- src/components/SettingsPopup.js
+++ src/components/SettingsPopup.js
@@ -2,35 +2,23 @@ import React, { useRef, useState, useEffect } from 'react';
 import { Dialog } from 'primereact/dialog';
 import { Button } from "primereact/button";
 import { useUser } from './UserProvider';
-import { Toast } from 'primereact/toast';
 import { Dropdown } from 'primereact/dropdown';
 import styles from "@/styles/Home.module.css";
 import { updateUserPreferences } from '@/utils/firebase';
 
 export default function SettingsPopup(props) {
     const [visible, setVisible] = useState(false);
-    const toast = useRef(null);
     const { user, darkMode, setDarkMode, units, setUnits } = useUser();
+    const [userIdCopied, setUserIdCopied] = useState(false);
 
     const handleCopy = (copyText) => {
         navigator.clipboard.writeText(copyText)
-            .then(() => showToast("Copied!"));
+            .then(() => {
+                setUserIdCopied(true);
+                setTimeout(() => setUserIdCopied(false), 2000);
+            });
     };
 
-    const showToast = (text, severity = 'success') => {
-        toast.current.show({
-            severity: severity,
-            summary: text,
-            life: 2000,
-            content: (
-                <div style={{ flex: '1' }}>
-                    <div style={{ padding: '12px' }}>
-                        <div>Copied!</div>
-                    </div>
-                </div>
-            ),
-        });
-    }
 
     const renderCopyUserIdButton = () => {
         const domain = window.location.hostname;
@@ -38,8 +26,14 @@ export default function SettingsPopup(props) {
         const text = `${domain}${port}/${user.id}`;
         return (
             <button className={styles.copyUserIdButton} onClick={() => handleCopy(text)}>
-                {text}
-                <i className="pi pi-copy" style={{ marginLeft: '8px' }}></i>
+                {userIdCopied ? (
+                    <span> Copied! </span>
+                ) : (
+                    <>
+                        {text}
+                        <i className="pi pi-copy" style={{ marginLeft: '8px' }}></i>
+                    </>
+                )}
             </button>
         );
     }
@@ -73,7 +67,7 @@ export default function SettingsPopup(props) {
                     onChange={(e) => e.value !== value && onChange(e)}
                     style={{ width: '100px', padding: '4px', borderRadius: '0' }}
                     itemTemplate={(option) => (
-                        <div style={{ padding: '4px'}}>
+                        <div style={{ padding: '4px' }}>
                             {option.label}
                         </div>
                     )}
@@ -108,12 +102,10 @@ export default function SettingsPopup(props) {
                 style={{ width: "100%", maxWidth: "400px", height: "100%" }}
                 headerStyle={{ height: '48px', padding: '12px' }}
             >
-                <div style={{ width: "100%", padding: "24px", display: 'flex', flexDirection: 'column', gap: '24px'}} >
+                <div style={{ width: "100%", padding: "24px", display: 'flex', flexDirection: 'column', gap: '24px' }} >
                     {renderCopyUserIdButton()}
                     {renderUserPreferencesSettings()}
                 </div>
-
-                <Toast ref={toast} position="top-center" />
             </Dialog>
 
         </>
